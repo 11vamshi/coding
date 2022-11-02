b3fb051acf1e67b6fcd2a2dcfc98db32860b355f
Vamshi Krishna Patha <vpatha@ford.com>
2022-11-02 13:05:10 -0400

    CPPNAVIG-1004 ford msxi practise

--- src/main/java/com/vamshi/coding/ford/MaximumRatingSum.java
+++ src/main/java/com/vamshi/coding/ford/MaximumRatingSum.java
@@ -1,2 +1,42 @@
-package com.vamshi.coding.ford;public class MaximumRatingSum {
+package com.vamshi.coding.ford;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MaximumRatingSum {
+
+    public static void main(String[] args) {
+        int[] ratings = {-1,3,4,-2,5,-7};
+
+        List<Integer> arr = Arrays.stream(ratings).boxed().collect(Collectors.toList());
+
+        System.out.println("maximum contiguous sum is " + maximumSum(arr));
+    }
+
+
+    // 2 5 7
+
+    // 1st iteration = 2, 2
+    // 2nd = 7,7
+    // 3rd = 14, 14
+
+
+    //2 -1 7 -1
+
+    // 1st iteration = 2, 2
+    // 2nd = -1,2
+    // 3rd = 7, 7
+    public static long maximumSum(List<Integer> list) {
+
+        int sumSoFar = 0;
+        long maxSum = 0;
+        for(int i =0; i < list.size(); i++){
+            sumSoFar = Math.max(sumSoFar + list.get(i), list.get(i) );
+            maxSum = Math.max(maxSum, sumSoFar);
+        }
+        return maxSum;
+
+    }
 }
--- src/main/java/com/vamshi/coding/ford/ShapeFactory.java
+++ src/main/java/com/vamshi/coding/ford/ShapeFactory.java
@@ -1,2 +1,44 @@
-package com.vamshi.coding.ford;public class ShapeFactory {
+package com.vamshi.coding.ford;
+
+public class ShapeFactory {
+    Shape sh;
+
+    // at ford, I implemented this factory with return type as EU or NA image service based on request
+    public void drawShape(String string) {
+
+
+        if(string.equals("Circle")) {
+            sh = new Circle();
+            sh.drawShape();
+        }else if(string.equals("Rectangle")){
+            sh = new Rectangle();
+            sh.drawShape();
+        }else if(string.equals("Square")){
+            sh = new Square();
+            sh.drawShape();
+        }else{
+            System.out.println("Drawing "+ string+" is not supported");
+        }
+    }
+}
+
+
+abstract class Shape{
+    public abstract void drawShape();
+}
+class Circle extends Shape{
+
+    public void drawShape(){
+        System.out.println("Drawing a Circle");
+    }
+}
+class Rectangle extends Shape {
+    public void drawShape() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
+class Square extends Shape{
+    public void drawShape() {
+        System.out.println("Drawing a Square");
+    }
 }
