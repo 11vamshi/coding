689e740abb8638815f78d402165124799154db9b
Vamshi Krishna Patha <vpatha@ford.com>
2022-11-02 23:25:06 -0400

    CPPNAVIG-1004 songs length divisible by 60

--- src/main/java/com/vamshi/coding/challenges/L1010_SongsDuration60.java
+++ src/main/java/com/vamshi/coding/challenges/L1010_SongsDuration60.java
@@ -1,2 +1,26 @@
-package com.vamshi.coding.challenges;public class L1010_SongsDuration60 {
+package com.vamshi.coding.challenges;
+
+import java.util.Arrays;
+
+public class L1010_SongsDuration60 {
+
+    public int numPairsDivisibleBy60(int[] time) {
+
+        Arrays.sort(time);
+
+        for(int i=0; i < time.length;i++){
+            
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] time = {30,20,150,100,40};
+
+        L1010_SongsDuration60 l1010_songsDuration60 = new L1010_SongsDuration60();
+
+        l1010_songsDuration60.numPairsDivisibleBy60(time);
+
+    }
 }
